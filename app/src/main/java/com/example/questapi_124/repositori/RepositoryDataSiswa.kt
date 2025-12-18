119eca263c3ecb58af1fd4d81e739b48b250a879
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-12-18 20:46:47 +0700

    Menambahkan Top App Bar

--- app/src/main/java/com/example/questapi_124/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_124/repositori/RepositoryDataSiswa.kt
@@ -3,15 +3,16 @@ package com.example.questapi_124.repositori
 import com.example.questapi_124.apiservice.ServiceApiSiswa
 import com.example.questapi_124.modeldata.DataSiswa
 
+
 interface RepositoryDataSiswa {
-    suspend fun getDataSiswa() : List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
 
-class JaringanRepositoryDataSiswa(
-    private  val serviceApiSiswa: ServiceApiSiswa
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
-    override suspend fun  getDataSiswa() : List<DataSiswa> = serviceApiSiswa
-        .getSiswa()
-    override suspend fun  postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
 }
